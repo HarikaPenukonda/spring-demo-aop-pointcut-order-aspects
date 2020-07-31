f58794a97032004489694ce99a05e5ac4d0bdb63
harika penukonda <penukondaharika@gmail.com>
2020-07-31 14:45:41 -0500

    Add order annotation to Aspects

--- src/com/udemy/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/com/udemy/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.udemy.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
 	
 	@Before("com.udemy.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()") // Apply pointcut declaration to advice
--- src/com/udemy/aopdemo/aspect/MyCloudLoadAsyncAspect.java
+++ src/com/udemy/aopdemo/aspect/MyCloudLoadAsyncAspect.java
@@ -2,11 +2,13 @@ package com.udemy.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class MyCloudLoagAsyncAspect {
+@Order(1)
+public class MyCloudLoadAsyncAspect {
 	
 	@Before("com.udemy.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()") // Apply pointcut declaration to advice
 	public void LogToClundAsync() 
